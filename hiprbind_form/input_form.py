58e6c30ee82cafd45938309fc33dd5e7728b712a
esimonds09 <esimonds@abscibio.com>
2021-05-11 17:13:06 -0700

    display updates to input form

--- hiprbind_form/input_form.py
+++ hiprbind_form/input_form.py
@@ -74,6 +74,7 @@ class InputForm:
         )
 
     def on_button_click(self, event):
+        self.stc_out.clear_output()
         self.output.clear_output()
         all_valid = True
         with open('parser_data.json', 'r') as parser_file:
@@ -83,9 +84,9 @@ class InputForm:
         point = self.point_choice.value
 
         if ',' in self.plates.value:
-            plate_ids = list(map(str.strip, self.plates.value.split(',')))
+            plate_ids = list(map(str.strip, self.plates.value.upper().split(',')))
         else:
-            plate_ids = list(map(str.strip, self.plates.value.split()))
+            plate_ids = list(map(str.strip, self.plates.value.upper().split()))
 
         for plate in plate_ids:
             if plate[:1].upper() == "P":
@@ -140,31 +141,36 @@ class InputForm:
             self.std_dict_all = {}
 
             with self.output:
-                display(ipw.HTML("<b>*** Project Updated ***</b>"))
+                display(ipw.HTML("<h4><b>*** Project Updated ***</b></h4>"))
                 # print('***  Project Updated  ***')
                 for proj, inner in proj_dict_all.items():
-                    display(ipw.HTML(f"<b>Project name entered:</b> {proj}"),
-                            ipw.HTML(f"<b>Plate Ids:</b> {', '.join(inner['plates'])}"),
-                            ipw.HTML(f"<b>Point Scheme:</b> {inner['points']}"),
-                            ipw.HTML(f"<b>Dilution Volumes:</b> {self.d_vols.value}"),
-                            ipw.HTML(f"<b>Add OD data:</b> {inner['od_file']}"),
-                            ipw.HTML("<b>Standards:</b>"))
+                    p_name_text = ipw.HTML(f"<span style='color: #7EAA31';><b>Project name entered:</b></span> {proj}")
+                    p_id_text = ipw.HTML(f"<span style='color: #7EAA31';><b>Plate Ids:</b></span> {', '.join(inner['plates'])}")
+                    points_text = ipw.HTML(f"<span style='color: #7EAA31';><b>Point Scheme:</b></span> {inner['points']}")
+                    dv_text = ipw.HTML(f"<span style='color: #7EAA31';><b>Dilution Volumes:</b></span> {self.d_vols.value}")
+                    od_text = ipw.HTML(f"<span style='color: #7EAA31';><b>Add OD data:</b></span> {inner['od_file']}")
+                    std_text = ipw.HTML("<span style='color: #7EAA31';><b>Standards:</b></span>")
+                    display_box = ipw.VBox([p_name_text, p_id_text, points_text, dv_text, od_text, std_text])
+                    display(display_box)
                     for key, value in inner['std_conc'].items():
-                        labels = ipw.HTML(f"<ul><li style='line-height:1px';><b>Well ID:</b> {key}, <b>Standard Concentration:</b> {value}</li></ul>")
+                        labels = ipw.HTML(f"<ul><li style='line-height:1px';><span style='color: #7EAA31';><b>Well ID:</b></span>"
+                                          f" {key}, <span style='color: #7EAA31';><b>Standard Concentration:</b></span>"
+                                          f" {value}</li></ul>")
                         display(labels)
             #             for key, value in inner.items():
             #                 print(f'{key} entered: {value}\n')
             return self.std_dict_all
         else:
             with self.output:
+                display(ipw.HTML("<h4><u><b>Please resolve the issues listed below!</b></u></h4>"))
                 if fix_plate == "No issues":
-                    display(ipw.HTML(f"<font color='green'>Plate IDs: {fix_plate}"))
+                    display(ipw.HTML(f"<b>Plate IDs: <font color='green'>{fix_plate}</b>"))
                 else:
-                    display(ipw.HTML(f"<b><font color='red'>Plate IDs: {fix_plate}</b>"))
+                    display(ipw.HTML(f"<b>Plate IDs: <font color='red'>{fix_plate}</b>"))
                 if fix_vols == "No issues":
-                    display(ipw.HTML(f"<font color='green'>Dilution Volumes: {fix_vols}"))
+                    display(ipw.HTML(f"<b>Dilution Volumes: <font color='green'>{fix_vols}</b>"))
                 else:
-                    display(ipw.HTML(f"<b><font color='red'>Dilution Volumes: {fix_vols}</b>"))
+                    display(ipw.HTML(f"<b>Dilution Volumes: <font color='red'>{fix_vols}</b>"))
 
     def add_standard(self, event):
         self.stc_out.clear_output()
@@ -215,75 +221,24 @@ class InputForm:
                 std_dict_all.update(std_dict)
             else:
                 with self.stc_out:
-                    display(ipw.HTML("<b>*** Standard Updated ***</b>"))
+                    display(ipw.HTML("<h4><b>*** Standard Updated ***</b></h4>"))
                     for key, value in self.std_dict_all.items():
                         labels = ipw.HTML(
-                            f"<ul><li style='line-height:1px';><b>Well ID:</b> {key}, <b>Standard Concentration:</b> {value}</li></ul>")
+                            f"<ul><li style='line-height:1px';><span style='color: #7EAA31';><b>Well ID:</b></span>"
+                            f" {key}, <span style='color: #7EAA31';><b>Standard Concentration:</b></span>"
+                            f" {value}</li></ul>")
                         display(labels)
                     display(ipw.HTML("<b>To add replicate, change well id, or 'column/row' to indicate new standard curve position.</b>"))
                 return self.std_dict_all
         else:
             with self.stc_out:
+                title = ipw.HTML("<h4><u><b>Please resolve the issues listed below!</b></u></h4>")
+                display(title)
                 if fix_well == "No issues":
-                    display(ipw.HTML(f"<font color='green'>Well ID: {fix_well}"))
+                    display(ipw.HTML(f"<b>Well ID: <font color='green'>{fix_well}</b>"))
                 else:
-                    display(ipw.HTML(f"<b><font color='red'>Well ID: {fix_well}</b>"))
+                    display(ipw.HTML(f"<b>Well ID: <font color='red'>{fix_well}</b>"))
                 if fix_conc == "No issues":
-                    display(ipw.HTML(f"<font color='green'>Standard concentrations: {fix_conc}"))
+                    display(ipw.HTML(f"<b>Standard concentrations: <font color='green'>{fix_conc}</b>"))
                 else:
-                    display(ipw.HTML(f"<b><font color='red'>Standard concentrations: {fix_conc}</b>"))
-
-# class StandardForm(InputForm):
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.loc = ipw.Dropdown(options=["Column", "Row"])
-#         self.well_loc = ipw.Text()
-#         self.std_conc = ipw.Text()
-#         self.add_stc_button = ipw.Button(description='Add Standard', button_style='info')
-#         self.stc_out = ipw.Output()
-#         self.display_standard()
-#         self.add_stc_button.on_click(self.add_standard)
-#
-#     def display_standard(self):
-#         display(
-#             ipw.Label("Add Standard information here:"),
-#             ipw.Label("Choose if standard curve was place in row or column:"),
-#             self.loc,
-#             ipw.Label("Enter the location of first well used, e.g., A11, or H01:"),
-#             self.well_loc,
-#             ipw.Label("Enter the standard curve concentrations, match first concentration with well"),
-#             self.std_conc,
-#             self.add_stc_button,
-#             self.stc_out
-#         )
-#
-#     def add_standard(self, event):
-#         self.stc_out.clear_output()
-#
-#         std_conc_list = [float(x) for x in self.std_conc.value.split(',')]
-#         std_conc_list_len = len(std_conc_list)
-#         if self.loc.value == 'Column':
-#             col_letter = self.well_loc.value[:1]
-#             letter_idx = upstr.index(col_letter)
-#             col_num = self.well_loc.value[1:]
-#             std_ids = [f"{upstr[letter]}{str(col_num).zfill(2)}" for letter in range(letter_idx, std_conc_list_len)]
-#             std_dict = (dict(zip(std_ids, std_conc_list)))
-#
-#         elif self.loc.value == 'Row':
-#             row_letter = self.well_loc.value[:1]
-#             row_num = int(self.well_loc.value[1:])
-#             std_ids = [f"{row_letter}{str(num).zfill(2)}" for num in range(row_num, row_num + std_conc_list_len)]
-#             std_dict = dict(zip(std_ids, std_conc_list))
-#         try:
-#             self.std_dict_all.update(std_dict)
-#         except AttributeError:
-#             std_dict_all = {}
-#             std_dict_all.update(std_dict)
-#         else:
-#             with self.stc_out:
-#                 for key, value in self.std_dict_all.items():
-#                     print(f'Well ID: {key}, Concentration: {value}')
-#                 print("\nTo add replicate, change well id, or 'column/row' to indicate new standard curve position.\n")
-#             return self.std_dict_all
-
+                    display(ipw.HTML(f"<b>Standard concentrations: <font color='red'>{fix_conc}</b>"))
