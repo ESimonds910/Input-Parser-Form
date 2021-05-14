c8621ecd03e1e9875b08b0fcbc0d430a8e134cfc
esimonds09 <esimonds@abscibio.com>
2021-05-14 14:16:46 -0700

    Small updates

--- hiprbind_parser/file_split.py
+++ hiprbind_parser/file_split.py
@@ -11,36 +11,37 @@ def split_projects(proj_dict):
     window.withdraw()
 
     file_path_raw = askopenfilename(title='Select file to copy', initialdir="L:/Assay Development/Enspire")
-    for proj, inner_dict in proj_dict.items():
-        proj_name = proj
-        plate_num = len(inner_dict["plates"])
-        try:
-            folder_path_raw = askdirectory(
-                title="Choose Raw folder from Project folder to place file for " + proj_name,
-                initialdir='L:/Molecular Sciences/Small Scale Runs'
-            )
-        except FileNotFoundError:
-            pass
-        else:
-            print('%s and %s' % (proj_name, plate_num))
-            with open(file_path_raw) as csv_file:
-                reader = csv.reader(csv_file)
-                copy_end_row += int(plate_num) * 48
-                plate_rows = [row for idx, row in enumerate(reader) if idx in range(copy_start_row, copy_end_row)]
-                copy_start_row = copy_end_row
+    if file_path_raw:
+        for proj, inner_dict in proj_dict.items():
+            proj_name = proj
+            plate_num = len(inner_dict["plates"])
+            try:
+                folder_path_raw = askdirectory(
+                    title="Choose Raw folder from Project folder to place file for " + proj_name,
+                    initialdir='L:/Molecular Sciences/Small Scale Runs'
+                )
+            except FileNotFoundError:
+                pass
+            else:
+                if folder_path_raw:
+                    with open(file_path_raw) as csv_file:
+                        reader = csv.reader(csv_file)
+                        copy_end_row += int(plate_num) * 48
+                        plate_rows = [row for idx, row in enumerate(reader) if idx in range(copy_start_row, copy_end_row)]
+                        copy_start_row = copy_end_row
 
-                raw_output = folder_path_raw + '/' + proj_name + '.csv'
-                with open(raw_output, 'w', newline="") as newFile:
-                    csv_writer = csv.writer(newFile)
-                    for row in plate_rows:
-                        csv_writer.writerow(row)
+                        raw_output = folder_path_raw + '/' + proj_name + '.csv'
+                        with open(raw_output, 'w', newline="") as newFile:
+                            csv_writer = csv.writer(newFile)
+                            for row in plate_rows:
+                                csv_writer.writerow(row)
 
-                inner_dict["raw_file"] = raw_output
+                        inner_dict["raw_file"] = raw_output
 
-    messagebox.showinfo(title="Congrats!", message="File has been moved!")
-    window.destroy()
+                        messagebox.showinfo(title="Congrats!", message="File has been moved!")
+                        window.destroy()
 
-    return proj_dict
+                        return proj_dict
 
 
 if __name__ == "__main__":
